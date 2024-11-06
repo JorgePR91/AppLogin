6fc0ccf9a4eeab305034008f0e95bbe3f72af574
JorgePR91 <georgos14@gmail.com>
2024-11-06 16:25:28 +0100

    Arreglo

--- login.php
+++ login.php
@@ -1,9 +1,9 @@
 
 <?php 
-$nomUsuari = $_POST['nom'] ="";
-$congnomsusuari = $_POST['cognoms'] ="";
-$contrasenya = $_POST['password'] = "";
-$correu = $_POST['email'] = "";
+$nomUsuari = $_POST['nom'];
+$congnomsusuari = $_POST['cognoms'];
+$contrasenya = $_POST['password'];
+$correu = $_POST['email'];
 echo "Formulari enviat!";
-echo "Nom de l'usuari: " . htmlspecialchars($nomUsuari) . " " . htmlspecialchars($congnomsusuari);
+echo "Nom de l'usuari: " . htmlspecialchars($nomUsuari) . " " . htmlspecialchars($congnomsusuari) . "<br>";
 echo "Contrasenya triada: " . htmlspecialchars($contrasenya); 
