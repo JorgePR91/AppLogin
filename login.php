489c43862134f1c7e2d7c75a9d478e208038af0e
JorgePR91 <georgos14@gmail.com>
2024-11-07 10:04:07 +0100

    Update login.php

--- login.php
+++ login.php
@@ -4,6 +4,6 @@ $nomUsuari = $_POST['nom'];
 $congnomsusuari = $_POST['cognoms'];
 $contrasenya = $_POST['password'];
 $correu = $_POST['email'];
-echo "Formulari enviat!";
+echo "Formulari enviat!<br>";
 echo "Nom de l'usuari: " . htmlspecialchars($nomUsuari) . " " . htmlspecialchars($congnomsusuari) . "<br>";
 echo "Contrasenya triada: " . htmlspecialchars($contrasenya); 
