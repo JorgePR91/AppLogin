cc1fcdd245a67b65303c5e411c43a0df67ba19e7
JorgePR91 <georgos14@gmail.com>
2024-12-09 16:29:46 +0100

    Canvi Codi

--- login_echo.php
+++ login_echo.php
@@ -4,6 +4,18 @@ $nomUsuari = $_POST['nom'];
 $congnomsusuari = $_POST['cognoms'];
 $contrasenya = $_POST['password'];
 $correu = $_POST['email'];
-echo "Formulari enviat!<br>";
-echo "Nom de l'usuari: " . $nomUsuari . " " . $congnomsusuari . "<br>";
-echo "Contrasenya triada: " . $contrasenya; 
+?>
+<!DOCTYPE html>
+<html lang="ca">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Alta usuari</title>
+</head>
+
+<body>
+    <h1>Formulari enviat!</h1><br>
+<p>Nom de l'usuari: <?php echo $nomUsuari." ". $congnomsusuari; ?><br></p>
+<p> Contrasenya triada: <?php echo  $contrasenya; ?> <br></p>
+</body>
+</html>
